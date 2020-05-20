9ef0be025facea2f66445ec15f1b5dad6c2405c1
Henry Doce <henrydoce@protonmail.com>
2020-05-20 11:17:48 -0400

    First commit

---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Henry Doce on 5/13/20.
+//  Copyright © 2020 Hyperfocus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
