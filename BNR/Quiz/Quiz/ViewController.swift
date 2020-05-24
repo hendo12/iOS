42132885a5527b587512c6fcf19c4b9654fd8eee
Henry Doce <henrydoce@protonmail.com>
2020-05-23 20:40:02 -0400

    Chapter 1 pg. 22

---
+++ BNR/Quiz/Quiz/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Quiz
+//
+//  Created by Henry Doce on 5/23/20.
+//  Copyright © 2020 Hyperfocus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var questionLabel: UILabel!
+    @IBOutlet var answerLabel: UILabel!
+    
+    @IBAction func showNextQuestion(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func showAnswer(_ sender: UIButton) {
+        
+    }
+}
+
