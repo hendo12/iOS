cc313672c98ce74e05512ed7352b8d787dc51e87
Henry Doce <henrydoce@protonmail.com>
2020-05-25 19:41:31 -0400

    Ch 3 pg 68 starting Auto Layout

---
+++ BNR/WorldTrotter/WorldTrotter/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  WorldTrotter
+//
+//  Created by Henry Doce on 5/25/20.
+//  Copyright © 2020 Hyperfocus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+}
+
